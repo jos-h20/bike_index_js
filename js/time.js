141733971e797c7f55c0fa3c2ca503abfc15f3e4
Josh Overly and Jon Sweeney <jon@>
2016-03-17 15:16:42 -0700

    moved to script page

---
+++ js/time.js
@@ -0,0 +1,12 @@
+exports.time = function (unix_time) {
+  var a = new Date(unix_time * 1000);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  return time;
+};
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,5 +1,6 @@
 var MyBike = require('./../js/mybike.js').MyBike;
 var moment = require('moment');
+var time = require('./../js/time.js').time;
 // var bike_query = $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&per_page=100&proximity_square=200&stolen_after=0');
 var apiKey = require('./../.env').apiKey;
 
@@ -14,35 +15,18 @@ $(document).ready(function(){
 
     var myBike = new MyBike(my_manufacture, my_serial, my_location);
 
-    function convertTimeStamp(unix_time) {
-      var a = new Date(unix_time * 1000);
-      var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-      var year = a.getFullYear();
-      var month = months[a.getMonth()];
-      var date = a.getDate();
-      var hour = a.getHours();
-      var min = a.getMinutes();
-      var sec = a.getSeconds();
-      var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-      return time;
-    }
-
     $.get(apiKey).then(function(response) {
       var bikes_array = response.bikes;
       console.log(bikes_array);
         var stolen_bike = [];
-        var manufacture_count = 0;
         for(var i = 0; i < bikes_array.length; i++) {
           var result = myBike.isStolen(response.bikes[i].serial);
-          manufacture_count += myBike.countManufacture(response.bikes[i].manufacturer_name);
           if(result) {
             stolen_bike.push(response.bikes[i]);
           }
         }
-        console.log(response.bikes[0].manufacturer_name);
-        console.log(manufacture_count);
         if(stolen_bike[0] !== undefined) {
-          var date_stolen = convertTimeStamp(stolen_bike[0].date_stolen);
+          var date_stolen = time(stolen_bike[0].date_stolen);
           $('#bike-stolen').text("Your bike was stolen in " + stolen_bike[0].stolen_location + " at " + date_stolen);
           alert('Your Bike was stolen');
         } else {
